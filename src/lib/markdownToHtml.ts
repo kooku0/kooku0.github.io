348ea530d44700dce9cecf84bd928ec07758752b
kooku <koomg9599@gmail.com>
2021-05-07 01:50:36 +0900

    feat: add remark-gfm for markdown-table

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,12 +1,14 @@
 import rehypePrism from '@mapbox/rehype-prism';
 import remarkStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import unified from 'unified';
 
-export default async function markdownToHtml(markdown: string) {
+export default async function markdownToHtml(markdown: string): Promise<string> {
   const result = await unified()
     .use(remarkParse)
+    .use(remarkGfm)
     .use(remarkRehype)
     .use(rehypePrism)
     .use(remarkStringify)
