e0288e0587f88ff6fb3f19f9422e84473019de18
kooku <koomg9599@gmail.com>
2021-05-09 02:27:04 +0900

    feat: add google tag manager script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,19 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>{metaConfig.title}</title>
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KDDR59L');
+              `
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
         {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-88SF4487LG" />
         <script
@@ -31,8 +44,18 @@ function MyApp({ Component, pageProps }: AppProps) {
               `
           }}
         />
+        {/* <!-- End Global site tag (gtag.js) - Google Analytics --> */}
       </Head>
-
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KDDR59L"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
       <Global styles={GlobalStyle} />
       <ThemeProvider theme={theme}>
         <React.StrictMode>
