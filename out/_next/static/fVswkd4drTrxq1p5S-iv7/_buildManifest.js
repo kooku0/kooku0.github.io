d438bfe643a6d4915acb9da783ce4e74936e176a
kooku <koomg9599@gmail.com>
2021-06-28 17:37:41 +0900

    Add out dir

---
+++ out/_next/static/fVswkd4drTrxq1p5S-iv7/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = (function (a) {
+  return {
+    __rewrites: [],
+    '/': [a, 'static\u002Fchunks\u002Fpages\u002Findex-58cf4165ed00e185b94b.js'],
+    '/_error': ['static\u002Fchunks\u002Fpages\u002F_error-808372eec74af5a95559.js'],
+    '/posts/[slug]': [
+      'static\u002Fchunks\u002Fb2e984c5.90ae03e7a268bc321c71.js',
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fposts\u002F[slug]-4b1f8797368aabf6c81f.js'
+    ],
+    sortedPages: ['\u002F', '\u002F_app', '\u002F_error', '\u002Fposts\u002F[slug]']
+  };
+})('static\u002Fchunks\u002F2c3b9bffc7150b37a7b3810dfcf1c0b0fac2bd18.e79ac9050d56eda63c0b.js');
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
---
+++ out/_next/static/fVswkd4drTrxq1p5S-iv7/_ssgManifest.js
@@ -0,0 +1,2 @@
+self.__SSG_MANIFEST = new Set(['\u002F', '\u002Fposts\u002F[slug]']);
+self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB();
