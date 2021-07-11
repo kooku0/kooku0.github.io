335f1861e4a905d5cc6c67638ebe9af2e57baca4
kooku <koomg9599@gmail.com>
2021-07-11 18:19:34 +0900

    Build

---
+++ docs/_next/static/vXCZ0B-zaqG_Nty_bH4MG/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = (function (a) {
+  return {
+    __rewrites: [],
+    '/': [a, 'static\u002Fchunks\u002Fpages\u002Findex-2bdaf7d08a6492f37003.js'],
+    '/_error': ['static\u002Fchunks\u002Fpages\u002F_error-808372eec74af5a95559.js'],
+    '/posts/[slug]': [
+      'static\u002Fchunks\u002Fb2e984c5.dde205419b836c367968.js',
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fposts\u002F[slug]-610214db06f58418f77a.js'
+    ],
+    sortedPages: ['\u002F', '\u002F_app', '\u002F_error', '\u002Fposts\u002F[slug]']
+  };
+})('static\u002Fchunks\u002F2c3b9bffc7150b37a7b3810dfcf1c0b0fac2bd18.3c035e65c4e49f36a0e4.js');
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
