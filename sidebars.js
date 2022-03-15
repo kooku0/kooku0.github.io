062a46cf7d391ca582a86f86db7163d467bb2ed3
kooku <kooku@kakao.com>
2022-03-15 19:18:51 +0900

    docs: 08 경계간 매핑하기

--- sidebars.js
+++ sidebars.js
@@ -58,6 +58,8 @@ const sidebars = {
         'books/get-your-hands-dirty-on-clean-architecture/04',
         'books/get-your-hands-dirty-on-clean-architecture/05',
         'books/get-your-hands-dirty-on-clean-architecture/06',
+        'books/get-your-hands-dirty-on-clean-architecture/07',
+        'books/get-your-hands-dirty-on-clean-architecture/08',
       ]
     },
     {
