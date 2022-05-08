26fcc4dddc319fdd6e96b400140be54dd096bf53
gh-actions <actions@github.com>
2022-05-08 23:34:40 +0000

    Deploy website - based on fd926665e4347d0a19159a60e250b7266f297a7a

---
+++ en/assets/js/0bdc701e.2d85cf05.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkooku_log=self.webpackChunkkooku_log||[]).push([[6186],{3715:function(e){e.exports=JSON.parse('{"permalink":"/en/blog","page":1,"postsPerPage":26,"totalPages":1,"totalCount":26,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
